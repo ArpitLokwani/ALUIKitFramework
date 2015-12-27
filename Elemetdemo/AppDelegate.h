31f28168827a9775100aec73472d661b40f26ead
ArpitLokwani <arpit.lokwani@gmail.com>
2015-12-27 13:06:18 +0530

    Initial Commit

---
+++ Elemetdemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Elemetdemo
+//
+//  Created by Arpit Lokwani on 12/27/15.
+//  Copyright © 2015 Arpit Lokwani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
